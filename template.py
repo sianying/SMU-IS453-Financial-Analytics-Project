a0879e549d6551e987a94ff853800c7fd2eea6c8
gwenduhh <gsatuito.2019@scis.smu.edu.sg>
2022-03-04 16:38:13 +0800

    Test plotly template

---
+++ template.py
@@ -0,0 +1,66 @@
+from dash import Dash, dcc, html
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children='Dash: A web application framework for your data.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
+     html.Div(children=[
+        html.Label('Dropdown'),
+        dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'], 'Montréal'),
+
+        html.Br(),
+        html.Label('Multi-Select Dropdown'),
+        dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'],
+                     ['Montréal', 'San Francisco'],
+                     multi=True),
+
+        html.Br(),
+        html.Label('Radio Items'),
+        dcc.RadioItems(['New York City', 'Montréal', 'San Francisco'], 'Montréal'),
+    ], style={'padding': 10, 'flex': 1}),
+
+    
+
+    dcc.Graph(
+        id='example-graph-2',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
